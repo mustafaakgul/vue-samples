e54724c76da2c33005b6a100826531138716ec3a
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2023-07-31 22:25:28 +0300

    Initial commit

---
+++ Counter/VueCounter/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import { ref } from 'vue'
+const count = ref(0);
+
+const subToCount = () => {
+  count.value = count.value - 1;
+};
+
+const addToCount = () => {
+  count.value = count.value + 1;
+};
+
+</script>
+
+<template>
+  <main>
+    <h4>
+      Count is ...
+    </h4>
+    <h1>{{ count }}</h1>
+    <button @click="count--">-</button>
+    <button @click="count++">+</button>
+  </main>
+</template>
+
+<style scoped>
+  main{
+    background-color: aqua;
+  }
+</style>
---
+++ Vuetify/imageapp/imageapp/src/components/SideBar.vue
@@ -0,0 +1,31 @@
+<script setup>
+import {defineEmits} from 'vue';
+
+const emits = defineEmits(['selectOption'])
+const selectOption = (isColor) => {
+  emits("selectOption", isColor)
+}
+</script>
+
+<template>
+  <v-navigation-drawer
+    expand-on-hover
+    rail
+  >
+    <v-list>
+      <v-list-item
+          prepend-avatar="https://randomuser.me/api/portraits/women/85.jpg"
+          title="Sandra Adams"
+          subtitle="sandra_a88@gmailcom"
+      ></v-list-item>
+    </v-list>
+
+    <v-divider></v-divider>
+
+    <v-list density="compact" nav>
+      <v-list-item prepend-icon="mdi-image" title="Color Gallery" value="color" @click="selectOption(true)"/>
+      <v-list-item prepend-icon="mdi-dialpad" title="Gray Gallery" value="color" @click="selectOption(false)"/>
+    </v-list>
+
+  </v-navigation-drawer>
+</template>
