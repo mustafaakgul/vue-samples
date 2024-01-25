27ddc0b04198a343aa0e15c9215bfa8b5ad0fa5b
Mustafa Akgül <46718026+mustafaakgul@users.noreply.github.com>
2024-01-26 02:03:45 +0300

    Add imageapp

---
+++ ImageApp/src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+  import SideBar from './components/SideBar.vue'
+  import ImageGallery from "@/components/ImageGallery.vue";
+  import {ref} from "vue";
+
+  const isWithColor = ref(true)
+
+  const onSelectOption = (isColor) => {
+    isWithColor.value = isColor
+  }
+</script>
+
+<template>
+  <v-app>
+    <SideBar @selectOption="onSelectOption"/>
+    <v-main>
+      <ImageGallery :isWithColor="isWithColor"/>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data: () => ({
+    //
+  }),
+}
+</script>
